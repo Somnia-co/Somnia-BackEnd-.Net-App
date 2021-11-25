2260d4b63fa3ef594e13eb99087e238131ccc675
pemix09 <pemix09@onet.pl>
2021-11-25 18:53:24 +0100

    Added mongoose

---
+++ config/config.js
@@ -0,0 +1,5 @@
+let connectionString = 'mongodb+srv://pemix09:<password>@cluster0.e9sdv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+module.exports = {
+    db: connectionString
+};
--- database/database.js
+++ database/database.js
@@ -1,8 +1,11 @@
+import config from './config'
+
 //this will be class for database class
 class Database{
 
     constructor(){
-
+        this.mongoose = require("mongoose");
+        this.mongoDB = '';
     }
 
     Init(){
