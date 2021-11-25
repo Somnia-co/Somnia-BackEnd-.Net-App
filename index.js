af5d75ac6a4c9bb9333849914e7a7871067f09f7
pemix09 <pemix09@onet.pl>
2021-11-25 15:19:59 +0100

    Added products router

--- index.js
+++ index.js
@@ -3,9 +3,18 @@ const express = require('express');
 const port = 8080;
 
 const app = express();
+const productRouter = require('./routers/productRouter.js');
 
 app.get('/',(req,res) =>{
     res.status(200).send('hello world');
 })
 
-app.listen(port, ()=>{console.log('server started');});
+//it will route all http methods
+app.all('route all', (req, res) =>{
+    res.status(200).send('routed all');
+})
+
+//specify which router to use
+app.use('/products', productRouter);
+
+app.listen(port, ()=>{console.log('server started, listening at: '+port);});
