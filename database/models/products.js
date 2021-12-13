31c379e1ab8b5a7f32a4b0ade1f2d2f29f2d09c1
pemix09 <pemix09@onet.pl>
2021-12-13 11:56:16 +0100

    Added collection to database

--- database/models/products.js
+++ database/models/products.js
@@ -4,7 +4,8 @@ const productsSchema = mongoose.Schema({
     name: String,
     description: String,
     category: String,
-    productCode: Number
+    productCode: Number,
+    collection: String
 });
 
 const Product = mongoose.model('Product', productsSchema);
