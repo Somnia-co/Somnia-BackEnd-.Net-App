4ede17120e40a55f85128cc180bca15be28b7f47
pemix09 <pemix09@onet.pl>
2021-12-13 11:48:26 +0100

    Routes are ready

--- routers/favoriteRouter.js
+++ routers/favoriteRouter.js
@@ -1,4 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const Favorite = require('../database/models/favorites');
+
+router.get('/:userID',async(req, res) => {
+    try{
+        const favorites = await Favorite.find({userID: req.params.userID});
+        res.status(200).json(favorites);
+    }
+    catch(err){
+        res.status(500).json(err.message);
+    }
+});
+
+router.get('/:orderID',async(req, res) => {
+    try{
+        const favorite = await Favorite.findById(req.params.orderID);
+        res.status(200).json(favorite);
+    }
+    catch(err){
+        res.status(500).json(err.message);
+    }
+});
+
+// router.delete('/',async(req.res) => {
+
+// })
 
 module.exports = router;
--- routers/productRouter.js
+++ routers/productRouter.js
@@ -15,5 +15,25 @@ router.get('/',async (req,res) => {
         res.status(500).json({"message":err.message});
     }
 });
+router.delete('/:id',async (req, res) => {
+    try{
+        const products = await Product.findById(req.params.id);
+        res.status(200).json({"message":"user with id: "+req.params.id+" deleted"});
+    }
+    catch(err){
+        res.status(500).json({"message": err.message});
+    }
+})
+router.put('/update',async (req, res) => {
+    try{
+        const product = await Product.findById(req.body.id);
+        product.name = req.body.name;
+        product.description = req.body.description;
+        res.status(200).json({"message":"user with id: "+ req.body.id+" updated"});
+    }
+    catch{
+        res.tatus(500).json({"message":"User with id: "+ req.body.id+" not updated/error occured"});
+    }
+})
 
 module.exports = router;
